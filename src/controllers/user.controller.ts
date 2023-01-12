38b3319d94eddd1b2be00f1bc7d633fcdc409bd0
pierrelstan <stanleypl75@gmail.com>
2023-01-12 15:22:46 -0400

    fix: husky & eslint

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -16,9 +16,9 @@ const validateToken = (req: Request, res: Response, next: NextFunction) => {
 };
 
 const register = async (req: Request, res: Response, next: NextFunction) => {
-    let { email, password } = req.body; // see if user exists
+    const { email, password } = req.body; // see if user exists
     console.log(req.body);
-    let user = await User.findOne({
+    const user = await User.findOne({
         email,
     });
     if (user) {
--- src/models/cart.model.ts
+++ src/models/cart.model.ts
@@ -1,11 +1,11 @@
-import { Schema, model } from 'mongoose'
-import { ICart } from '../types/types'
+import { Schema, model } from 'mongoose';
+import { ICart } from '../types/types';
 
 const cartSchema = new Schema<ICart>({
-  userId: { type: String, require: true },
-  itemId: { type: Schema.Types.ObjectId, ref: 'Item' },
-  isAddedToCart: { type: Boolean, require: true },
-})
+    userId: { type: String, require: true },
+    itemId: { type: Schema.Types.ObjectId, ref: 'Item' },
+    isAddedToCart: { type: Boolean, require: true },
+});
 
-const Cart = model<ICart>('Cart', cartSchema)
-export default Cart
+const Cart = model<ICart>('Cart', cartSchema);
+export default Cart;
